47b00252bf0a81ad64fd85b1b893a48bcea8fea7
Ali Syahidin <ali.idinz@gmail.com>
2018-08-02 08:18:31 +0700

    initial simple route

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom';
 
 class App extends Component {
   render() {
@@ -13,9 +14,27 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <BrowserRouter>
+            <Switch>
+                <Route path='/contact' component={ Contact } />
+                <Route path='/about' component={ About } />
+                <Route exact path='/' component={ Home } />
+                <Redirect from='*' to='/' />
+            </Switch>
+        </BrowserRouter>
       </div>
     );
   }
 }
 
 export default App;
+
+const Home = props => {
+  return <h1>Home</h1>
+}
+const About = props => {
+  return <h1>About</h1>
+}
+const Contact = props => {
+  return <h1>Contact</h1>
+}

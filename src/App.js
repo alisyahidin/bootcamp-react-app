7f750fe3e5d9efc9b3a57900c1f03719f4fce7b9
Ali Syahidin <ali.idinz@gmail.com>
2018-08-08 20:42:47 +0700

    install antd-mobile

--- src/App.js
+++ src/App.js
@@ -2,20 +2,32 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import { NavBar, Icon } from 'antd-mobile';
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <NavbarComponent />
       </div>
     );
   }
 }
+
+export const NavbarComponent = () => {
+  return (
+    <div>
+      <NavBar
+        mode="dark"
+        leftContent="Back"
+        rightContent={[
+          <Icon key="0" type="search" style={{ marginRight: '16px' }} />,
+          <Icon key="1" type="ellipsis" />,
+        ]}
+      >NavBar</NavBar>
+    </div>
+  );
+};
+
 
 export default App;

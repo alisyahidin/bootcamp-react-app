fecad267f915be7209b34a15d1b81f4dd7bd9202
Ali Syahidin <ali.idinz@gmail.com>
2018-08-16 06:46:59 +0700

    config for API routes

---
+++ src/config/api.js
@@ -0,0 +1,6 @@
+const baseRoute = 'http://localhost:5000/api';
+const restaurantRoute = 'http://localhost:5000/api/restaurants';
+const foodRoute = 'http://localhost:5000/api/foods';
+const orderRoute = 'http://localhost:5000/api/orders';
+
+export { baseRoute, restaurantRoute, foodRoute, orderRoute }
